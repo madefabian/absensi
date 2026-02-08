89f9ae421b26ed32c27d7ee8a784fd10f93120e3
Made Fabian <madefabian882@gmail.com>
2026-02-08 19:30:18 +0700

    penambahan fungsi qr

---
+++ app/Filament/Resources/Rapats/Tables/RapatsTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\Rapats\Tables;
+
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ViewColumn;
+
+
+class RapatsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('judul')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('tanggal')
+                    ->date()
+                    ->sortable(),
+
+                TextColumn::make('jam_mulai'),
+                TextColumn::make('jam_selesai'),
+
+                TextColumn::make('lokasi'),
+
+                IconColumn::make('qr_status')
+                    ->label('QR Aktif')
+                    ->boolean(),
+                ViewColumn::make('qr_code')
+                    ->label('QR Code')
+                    ->view('filament.tables.columns.qr-code')
+
+            ]);
+    }
+}
