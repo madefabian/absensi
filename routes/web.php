82eaa8accd3131699ea150f930117efe24db26ab
Rasya <mrasyasaputra15@gmail.com>
2026-02-09 11:44:35 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\AbsensiController;
 Route::middleware('auth')->group(function () {
     Route::get('/absensi/{token}', [AbsensiController::class, 'scan'])
         ->name('absensi.scan');
-        
+
 });
 
 
 
 Route::get('/', function () {
     return view('welcome');
+
 });
