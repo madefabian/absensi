e0f07f11c00e9d84075cc416b89e236ffcae9915
Rasya <mrasyasaputra15@gmail.com>
2026-03-05 09:09:21 +0700

    mematikan fitur export pada absensi

--- app/Filament/Resources/Absensis/Pages/ListAbsensis.php
+++ app/Filament/Resources/Absensis/Pages/ListAbsensis.php
@@ -3,9 +3,9 @@
 namespace App\Filament\Resources\Absensis\Pages;
 
 use App\Filament\Resources\Absensis\AbsensiResource;
-use App\Filament\Exports\AbsensiExporter;
+//use App\Filament\Exports\AbsensiExporter;
 use Filament\Actions\CreateAction;
-use Filament\Actions\ExportAction;
+//use Filament\Actions\ExportAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAbsensis extends ListRecords
@@ -16,9 +16,9 @@ class ListAbsensis extends ListRecords
     {
         return [
             CreateAction::make(),
-            ExportAction::make()
-                ->exporter(AbsensiExporter::class)
-                ->label('Export Absensi'),
+            //ExportAction::make()
+                //->exporter(AbsensiExporter::class)
+                //->label('Export Absensi')
         ];
     }
 }
